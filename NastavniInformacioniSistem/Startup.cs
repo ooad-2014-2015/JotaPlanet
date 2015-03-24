9edb5e30561b2bc51ce330cb0b7797ebc66e6856
kprses <kprses1@etf.unsa.ba>
2015-03-24 19:51:15 +0100

    Template projekta (inicijalno kopiranje)

---
+++ NastavniInformacioniSistem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NastavniInformacioniSistem.Startup))]
+namespace NastavniInformacioniSistem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
